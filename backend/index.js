4b9ecfa7d650292bc3bb2cfbf998fcbd583f861d
Shubham <shubhamkhale301@gmail.com>
2023-07-04 23:54:35 +0530

    added controllers

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRouter = require('./routes/auth');
 const dotenv = require('dotenv').config()
-const app = express()
+const app = express();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
-    console.log('db connection is a success')
+    console.log('db connection is a success');
 })
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use('/auth', authRouter);
 
-app.listen(process.env.PORT, () => console.log('Server has been connected successfully'))
+app.listen(5000, () => console.log('Server has been connected successfully'))
