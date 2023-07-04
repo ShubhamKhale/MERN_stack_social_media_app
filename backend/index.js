7143d6f97fea254b67ffc22fc0597fe274205817
Shubham <shubhamkhale301@gmail.com>
2023-07-05 01:00:36 +0530

    added public dir and updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRouter = require('./routes/auth');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const authRouter = require('./routes/auth')
+const userRouter = require('./routes/user')
+const postRouter = require('./routes/post')
+const commentRouter = require('./routes/comment')
+const uploadRouter = require('./controllers/uploadController')
 const dotenv = require('dotenv').config()
-const app = express();
+const app = express()
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
-    console.log('db connection is a success');
+    console.log('db connection is a success')
 })
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use('/images', express.static('public/images'))
 
-app.use('/auth', authRouter);
+// middlewares
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-app.listen(5000, () => console.log('Server has been connected successfully'))
+app.use('/auth', authRouter)
+app.use('/user', userRouter)
+app.use('/post', postRouter)
+app.use('/comment', commentRouter)
+app.use('/upload', uploadRouter)
+
+app.listen(process.env.PORT, () => console.log('Server has been connected successfully'))
